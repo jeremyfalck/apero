d0f911d3e793f5e049c0ad35ae11e2a2a487f516
jeremyfalck <jeremy.falck@proton.me>
2023-01-05 18:03:14 +0100

    updated code

--- src/App.js
+++ src/App.js
@@ -5,46 +5,45 @@ import Times from "./time.json";
 function App() {
   const today = new Date();
 
-  var minutes = today.getMinutes();
+  const minutes = today.getMinutes();
+
+  var minutesString = minutes.toString();
 
   if (minutes < 10) {
-    minutes = "0" + minutes;
+    minutesString = "0" + minutesString;
   }
 
   const hours = today.getHours();
 
-  const time =
-    minutes != "00" ? hours + "h" + minutes : hours + "h" + " " + "pile";
+  const hourToDisplay =
+    minutesString == "45" ? ((hours + 1) % 24).toString() + "h" : hours + "h";
 
-  const isAperoTime =
-    (hours >= 18 && hours >= 20) || (hours >= 11 && hours <= 13);
+  if (minutesString === "00") {
+    minutesString = " pile";
+  } else if (minutesString === "15") {
+    minutesString = " et quart";
+  } else if (minutesString === "30") {
+    minutesString = " et demie";
+  } else if (minutesString === "45") {
+    minutesString = " moins le quart";
+  }
+
+  const time = hourToDisplay + minutesString;
 
   const value = Times[minutes] || "de boire";
 
   return (
     <div className="App">
       <header className="App-header">
-        {isAperoTime ? (
-          <p>Il est bien l'heure de l'apéro</p>
-        ) : (
-          <NoAperoTime time={time} value={value} />
-        )}
+        <p>
+          Normalement on prend l'apéro entre 18 et 20h. Mais en réalité il est
+          toujours l'heure de l'apéro.
+        </p>
+        <p>
+          La preuve: il est {time}, l'heure {value}
+        </p>
       </header>
     </div>
-  );
-}
-
-function NoAperoTime({ time, value }) {
-  return (
-    <>
-      <p>
-        Normalement on prend l'apéro entre 18 et 20h. Mais en réalité il est
-        toujours l'heure de l'apéro.
-      </p>
-      <p>
-        La preuve: il est {time}, l'heure {value}
-      </p>
-    </>
   );
 }
 
